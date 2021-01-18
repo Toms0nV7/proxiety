62267fcf0c6ba5507826328e0c97bbd9f0cc5a0e
williesaiki <46130595+williesaiki@users.noreply.github.com>
2021-01-18 13:18:03 +0100

    Update screen_xiety.sh

--- screen_xiety.sh
+++ screen_xiety.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
 yum install screen -y
-wget https://raw.githubusercontent.com/williesaiki/proxiety/main/full_xiety
+wget https://raw.githubusercontent.com/williesaiki/proxiety/main/full_xiety.sh
+chmod +x full_xiety.sh
 screen -dmS name ./full_xiety.sh
